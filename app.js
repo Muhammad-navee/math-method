4c1023756fc888830ed7bcb23254a076fc3a608a
muhammad naveed <amirprinceak5@gmail.com>
2022-06-11 20:39:05 +0500

    complete work

---
+++ app.js
@@ -0,0 +1,106 @@
+///// MATH METHODS
+
+//Q-1
+//A NUMBER 
+//var number = 3.45214;
+//document.write(number);
+
+//B ROUND OF VALUE
+
+//var number = 3.45214;
+//var roundValue = Math.round(number);
+//document.write(roundValue);
+
+//C FLOOR VALUE 
+//var number = 3.45214;
+//var floor = Math.floor(number);
+//document.write(floor);
+
+//D ceil  value
+//var number = 3.45214;
+//var ceil = Math.ceil(number);
+//document.write(ceil);
+
+//Q-2
+
+//A NUMBER 
+//var number = -2.673;
+//document.write(number);
+
+//B ROUND OF VALUE
+
+//var number = -2.673;
+//var roundValue = Math.round(number);
+//document.write(roundValue);
+
+//C FLOOR VALUE 
+//var number = -2.673;
+//var floor = Math.floor(number);
+//document.write(floor);
+
+//D ceil  value
+//var number = -2.673;
+//var ceil = Math.ceil(number);
+//document.write(ceil);
+
+//Q-3
+//ABSOLUTE VALUE -4 is 4 
+
+//var a = Math.abs(-4)
+//document.write(a);
+
+//ABSOLUTE VALUE 5 is 5
+
+ 
+// var a = Math.abs(5)
+// document.write(a);
+
+//Q-4
+// RANDOM VALUE 4
+
+//var a = Math.random() * 4 + 1
+//document.write(a);
+
+//RANDOM VALUE 6
+
+//var a = Math.random() * 6 + 1
+//document.write(a);
+
+//Q-5
+
+// var headsuser = prompt("enter heads username");
+// var tailuser = prompt("enter tails username");
+// var toss = Math.random() * 2;
+// var floor = Math.floor(toss)
+// if (floor === 0){
+//     alert("Heads " + headsuser + " win the toss")
+// }else{
+//     alert("Tails " + tailsuser + " win the toss")
+// }
+
+//Q-6
+// RANDOM NUMBER 1 AND 100
+//var a = Math.floor(Math.random() *100) +1;
+//document.write(a);
+
+//Q-7
+
+//var a = prompt("enter your weight in kgs");
+ //  document.write("your weight is " , a  ,);
+
+ //Q-8
+
+// alert("you have 10 gusses. guess 1-10");
+// var answer = 3;
+// var guess = prompt("Enter a number between 1 and 10 ");
+
+// for (i=0;i<10;i++){
+//     if (answer==guess){
+//         alert("you guessed correctly");
+//         break;
+//     }else{
+//         guess= prompt("please try again");
+//     }
+// }
+                                  
+                                     ///////// complete//////////////
